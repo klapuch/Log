2d8d78c1d1f5ae3fbec83ebfd82299540466e057
Dominik Klapuch <klapuchdominik@gmail.com>
2016-11-12 15:27:57 +0100

    Improved fakes

--- Core/FakeLogs.php
+++ Core/FakeLogs.php
@@ -6,7 +6,14 @@ namespace Klapuch\Log;
  * Fake
  */
 final class FakeLogs implements Logs {
-	public function put(Log $log): void {
+	private $location;
 
+	public function __construct(string $location = null) {
+		$this->location = $location;
+	}
+
+	public function put(Log $log): void {
+		if($this->location)
+			file_put_contents($this->location, $log->description());
 	}
 }
